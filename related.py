8f690e6c5941bb6856ef7afda5ccf1936461bacb
Danny Rosen <danny.rosen@gmail.com>
2013-04-22 14:23:07 -0400

    string formattting garh

--- related.py
+++ related.py
@@ -21,7 +21,7 @@ def evalField(new_ticket,response,editor,title):
     print "debug " + ticket_related
 
 def updateRelated(new_ticket,ticket_related,editor,title):
-    response = fogbugz.edit(ixBug=ticket_related,sEvent=('case %s (%s) has been opened by %s' % ticket_related, title, editor))
+    response = fogbugz.edit(ixBug=ticket_related,sEvent=('case %s (%s) has been opened by %s' % (ticket_related, title, editor))
 
 if hasattr(fbSettings,'TOKEN'):
     fogbugz = FogBugz(fbSettings.URL, fbSettings.TOKEN)
